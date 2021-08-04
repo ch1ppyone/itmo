809d54ede1a4dbd26f3e09ff224d1936028c3511
Ivan <pereyyra@rambler.ru>
2021-08-04 19:16:50 +0300

    fix burgers menu item highlight

--- scripts/script.js
+++ scripts/script.js
@@ -331,3 +331,19 @@ function showPopup(popup) {
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
+
+const burgerItem = document.querySelectorAll(".header__menu-link_type_popup");
+
+burgerItem.forEach((item) => {
+
+  item.addEventListener("click", (e) => {
+    burgerItem.forEach(item => {
+      item.style.fontWeight = '400';
+    });
+
+    item.style.cssText = 'font-weight: 600';
+  })
+});
+
+
+
